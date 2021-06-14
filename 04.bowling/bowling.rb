ff98f7b73e9afdd1d206bca17109e3a0c9ef2327
Megurin <tutimur_0521@yahoo.co.jp>
2021-06-14 15:53:31 +0900

    ボウリングプログラムの修正・リファクタリング

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -18,17 +18,11 @@ end
 
 # フレームごとに分割
 frames = []
-# shotsの数が20の時(最後のフレームが2投で終わる時)
-if shots.length == 20
-  shots.each_slice(2) do |s|
-    frames << s
-  end
-else # 最後のフレームが3投で終わる時
-  shots.first(18).each_slice(2) do |s|
-    frames << s
-  end
-  frames << shots.drop(18)
+# 10フレーム目は2投ではなく3投の可能性があること考慮
+shots.first(18).each_slice(2) do |s|
+  frames << s
 end
+frames << shots.drop(18)
 
 p frames
 
@@ -36,33 +30,29 @@ p frames
 # フレーム９番目と最終フレームのみ、加算方法が異なる
 point = 0
 
-frames.each.with_index do |frame, i|
-  case i
-  when 0..7 # フレーム１〜８の時
-    point +=
-      if frames[i].first == 10 && frames[i + 1].first == 10 # strikeが２連続
-        10 + 10 + frames[i + 2].first
-      elsif frames[i].first == 10 && frames[i + 1].first != 10 # strikeが１回のみ
-        10 + frame[0] + frame[1]
-      elsif frames[i].sum == 10 # spare
-        10 + frames[i + 1].first
-      else
-        frames[i].sum
-      end
-  when 8 # フレーム９の時
-    point +=
-      if frames[i].first == 10 && frames[i + 1][0] == 10 && frames[i + 1][2] == 10 # strikeが3連続
-        10 + 10 + 10
-      elsif frames[i].first == 10 && frames[i + 1][0] != 10 # strikeが１回のみ
-        10 + frame[0] + frame[1]
-      elsif frames[i].sum == 10 # spare
-        10 + frames[i + 1].first
-      else
-        frames[i].sum
-      end
-  else # 最終フレームの時
-    point += frames[i].sum # 最終フレームは全て足し算
-  end
+frames.each.with_index do |_frame, i|
+  point += case i
+           when 0..7 # フレーム１〜８の時
+             if frames[i][0] == 10 && frames[i + 1][0] == 10 # strikeが２連続
+               10 + 10 + frames[i + 2][0]
+             elsif frames[i][0] == 10 && frames[i + 1][0] != 10 # strikeが１回のみ
+               10 + frames[i + 1][0] + frames[i + 1][1]
+             elsif frames[i].sum == 10 # spare
+               10 + frames[i + 1][0]
+             else
+               frames[i].sum
+             end
+           when 8 # フレーム９の時
+             if frames[i][0] == 10 # strikeが出た時
+               10 + frames[i + 1][0] + frames[i + 1][1]
+             elsif frames[i].sum == 10 # spare
+               10 + frames[i + 1][0]
+             else
+               frames[i].sum
+             end
+           else # 最終フレームの時
+             frames[i].sum # 最終フレームは全て足し算
+           end
 end
 
 puts point
