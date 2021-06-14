48040deedbff8fe7d69dbe44dd07212aa2ec2c9e
Megurin <tutimur_0521@yahoo.co.jp>
2021-06-14 16:35:02 +0900

    フレーム9のストライク２連続の場合修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -41,8 +41,8 @@ frames.each.with_index do |_frame, i|
                frames[i].sum
              end
            when 8 # フレーム９の時
-             if frames[i].first == 10 && frames[i + 1][0] == 10 && frames[i + 1][2] == 10 # strikeが3連続
-               10 + 10 + 10
+             if frames[i].first == 10 && frames[i + 1][0] == 10  # strikeが2連続
+               10 + 10 + frames[i + 1][2]
              elsif frames[i].first == 10 && frames[i + 1][0] != 10 # strikeが１回のみ
                10 + frames[i + 1][0] + frames[i + 1][1]
              elsif frames[i].sum == 10 # spare
