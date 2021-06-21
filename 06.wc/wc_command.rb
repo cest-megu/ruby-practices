aca96999d7172d522971d336ea946b766946ee73
Megurin <tutimur_0521@yahoo.co.jp>
2021-06-22 00:28:41 +0900

    wc ファイル名の実装

---
+++ 06.wc/wc_command.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'irb'
+
+# コマンドラインの指定
+opt = OptionParser.new
+params = {}
+
+# オプションの引数が必須でないことを示すには、" [" を付ける
+opt.on('-l VAL') {|v| params[:l] = Dir.glob('*') }
+
+opt.parse!(ARGV)
+p ARGV # ファイル名を指定した時
+p params # params[:l] => ファイル名
+
+# binding.irb
+
+# wc ファイル名で、行数、単語数、バイト数、ファイル名を表す
+def wc_file
+  list = []
+  total_list = []
+  sum_of_line = 0
+  sum_of_words = 0
+  sum_of_bytesize = 0
+  ARGV.each do |file|
+    content_of_file = File.read(file)
+    line_count = content_of_file.lines.count # 行数
+    sum_of_line += line_count
+    num_of_words = content_of_file.split(/\s/).reject(&:empty?).size # 単語数
+    sum_of_words += num_of_words
+    bytesize = content_of_file.bytesize # バイト数
+    sum_of_bytesize += bytesize
+    # wc ファイル名の結果を配列に
+    list = [line_count.to_s.rjust(8), num_of_words.to_s.rjust(8), bytesize.to_s.rjust(8), file.rjust(30)]
+    puts list.join("")
+    total_list =[sum_of_line.to_s.rjust(8), sum_of_words.to_s.rjust(8), sum_of_bytesize.to_s.rjust(8), "total".rjust(13)]
+  end
+  puts total_list.join("")
+end
+
+wc_file
