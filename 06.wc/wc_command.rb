dbc3d2a2f6b5d9f525a8cdaa4668f0c3640815be
Megurin <tutimur_0521@yahoo.co.jp>
2021-06-24 17:18:08 +0900

    wc -lの指定と実行

--- 06.wc/wc_command.rb
+++ 06.wc/wc_command.rb
@@ -2,18 +2,22 @@
 require 'optparse'
 require 'irb'
 
-# コマンドラインの指定
-opt = OptionParser.new
-params = {}
-
-# オプションの引数が必須でないことを示すには、" [" を付ける
-opt.on('-l VAL') {|v| params[:l] = Dir.glob('*') }
-
-opt.parse!(ARGV)
-p ARGV # ファイル名を指定した時
-p params # params[:l] => ファイル名
-
-# binding.irb
+# 行数のみ
+def wc_l
+  list = []
+  total_list = []
+  sum_of_line = 0
+  ARGV.each do |file|
+    content_of_file = File.read(file)
+    line_count = content_of_file.lines.count # 行数
+    sum_of_line += line_count
+    # wc ファイル名の結果を配列に
+    list = [line_count.to_s.rjust(8), file]
+    puts list.join(" ")
+    total_list =[sum_of_line.to_s.rjust(8), "total"]
+  end
+  puts total_list.join(" ") if ARGV[1] != nil
+end
 
 # wc ファイル名で、行数、単語数、バイト数、ファイル名を表す
 def wc_file
@@ -33,10 +37,22 @@ def wc_file
     # wc ファイル名の結果を配列に
     list = [line_count.to_s.rjust(8), num_of_words.to_s.rjust(8), bytesize.to_s.rjust(8), file]
     puts list.join(" ")
-    # binding.irb
     total_list =[sum_of_line.to_s.rjust(8), sum_of_words.to_s.rjust(8), sum_of_bytesize.to_s.rjust(8), "total"]
   end
   puts total_list.join(" ") if ARGV[1] != nil
 end
 
-wc_file
+# コマンドラインの指定
+opt = OptionParser.new
+params = {}
+
+# オプションの引数
+opt.on('-l') {|v| params[:l] = v }
+
+opt.parse!(ARGV)
+p ARGV # ファイル名を指定した時
+p params # params[:l] => ファイル名
+
+params[:l] ? wc_l : wc_file
+
+binding.irb
