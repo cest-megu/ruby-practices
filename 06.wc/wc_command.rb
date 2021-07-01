eb59d40ee388cf90a9b19a408159dfefb4736c55
Megurin <tutimur_0521@yahoo.co.jp>
2021-07-01 15:36:58 +0900

    単語数取得のメソッドを修正

--- 06.wc/wc_command.rb
+++ 06.wc/wc_command.rb
@@ -9,7 +9,7 @@ end
 
 # 単語数を取得
 def words_count(str)
-  str.split(/\s/).reject(&:empty?).size
+  str.split(/\s+/).size
 end
 
 # バイト数を取得
@@ -25,10 +25,10 @@ def wc_l
   ARGV.each do |file|
     sum_of_line += line_count(File.read(file))
     list = [line_count(File.read(file)).to_s.rjust(8), file] # wc ファイル名の結果を配列に
-    puts list.join(" ")
-    total_list =[sum_of_line.to_s.rjust(8), "total"] # totalの結果
+    puts list.join(' ')
+    total_list = [sum_of_line.to_s.rjust(8), 'total'] # totalの結果
   end
-  puts total_list.join(" ") if ARGV[1] != nil
+  puts total_list.join(' ') unless ARGV[1].nil?
 end
 
 # wc ファイル名で、行数、単語数、バイト数、ファイル名を表す
@@ -44,10 +44,10 @@ def wc
     sum_of_bytesize += bytesize(File.read(file))
     # wc ファイル名の結果を配列に
     list = [line_count(File.read(file)).to_s.rjust(8), words_count(File.read(file)).to_s.rjust(8), bytesize(File.read(file)).to_s.rjust(8), file]
-    puts list.join(" ")
-    total_list =[sum_of_line.to_s.rjust(8), sum_of_words.to_s.rjust(8), sum_of_bytesize.to_s.rjust(8), "total"]
+    puts list.join(' ')
+    total_list = [sum_of_line.to_s.rjust(8), sum_of_words.to_s.rjust(8), sum_of_bytesize.to_s.rjust(8), 'total']
   end
-  puts total_list.join(" ") if ARGV[1] != nil
+  puts total_list.join(' ') unless ARGV[1].nil?
 end
 
 # 標準入力を受け取り、文字列でinputに保存する
@@ -63,11 +63,8 @@ end
 opt = OptionParser.new
 params = {}
 # オプションの引数
-opt.on('-l') {|v| params[:l] = v }
-
+opt.on('-l') { |v| params[:l] = v }
 opt.parse!(ARGV)
-p ARGV # ファイル名を指定した時
-p params # params[:l] => ファイル名
 
 if ARGV.empty? # ファイル指定なし（標準入力）
   show_data
