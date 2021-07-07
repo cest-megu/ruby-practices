00ad1d344c496d0840ff104887d45e2943ab5342
Megurin <tutimur_0521@yahoo.co.jp>
2021-07-07 10:14:16 +0900

    メソッドの命名変更、メソッドの順番変更

--- 06.wc/wc_command.rb
+++ 06.wc/wc_command.rb
@@ -7,7 +7,42 @@ def main
   opt.on('-l') { |v| params[:l] = v }
   opt.parse!(ARGV)
 
-  ARGV.empty? ? read_stdin : wc(ARGV, params[:l])
+  ARGV.empty? ? read_input(params[:l]) : output(ARGV, params[:l])
+end
+
+def read_input(params_boolean)
+  input = $stdin.read
+  print_values(count_line(input), count_words(input), count_bytesize(input), nil, params_boolean)
+end
+
+def output(files, params_boolean)
+  line_sum = 0
+  word_sum = 0
+  bytesize_sum = 0
+  files.each do |file|
+    input = File.read(file)
+    line_count = count_line(input)
+    line_sum += line_count
+    word_count = count_words(input)
+    word_sum += word_count
+    bytesize_count = count_bytesize(input)
+    bytesize_sum += bytesize_count
+    print_values(line_count, word_count, bytesize_count, file, params_boolean)
+  end
+  print_values(line_sum, words_sum, bytesize_sum, 'total', params_boolean) if files.size > 1
+end
+
+def print_values(line, words, bytesize, file_name, params_boolean)
+  print format_value(line)
+  unless params_boolean
+    print format_value(words)
+    print format_value(bytesize)
+  end
+  if file_name
+    print ' '
+    print file_name
+  end
+  print "\n"
 end
 
 def count_line(str)
@@ -24,39 +59,6 @@ end
 
 def format_value(value)
   value.to_s.rjust(8)
-end
-
-def print_values(line, words, bytesize, file_name, params)
-  print format_value(line)
-  unless params
-    print format_value(words)
-    print format_value(bytesize)
-  end
-  print ' '
-  print file_name
-  print "\n"
-end
-
-def read_stdin
-  input = $stdin.read
-  print_values(count_line(input), count_words(input), count_bytesize(input), nil, nil)
-end
-
-def wc(files, params)
-  line_sum = 0
-  words_sum = 0
-  bytesize_sum = 0
-  files.each do |file|
-    input = File.read(file)
-    line_count = count_line(input)
-    line_sum += line_count
-    words_count = count_words(input)
-    words_sum += words_count
-    bytesize_count = count_bytesize(input)
-    bytesize_sum += bytesize_count
-    print_values(line_count, words_count, bytesize_count, file, params)
-  end
-  print_values(line_sum, words_sum, bytesize_sum, 'total', params) if files.size > 1
 end
 
 main
